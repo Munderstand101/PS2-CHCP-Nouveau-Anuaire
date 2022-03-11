0d8b5b543bafe5c216c2e30b8bf997c225cad554
Munderstand101 <anchidins0@gmail.com>
2022-03-11 15:28:59 +0100

    changed registration route

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/admin/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
